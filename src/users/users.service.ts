d649db276985c4acf48a8e52cab186c5c12263b8
Lucas Correia <lucas_p_correia_cadmus@whirlpool.com>
2025-04-06 11:28:37 -0300

    feat: users and prisma module

---
+++ src/users/users.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import bcrypt from 'bcrypt';
+
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private prismaService: PrismaService) {}
+
+  create(createUserDto: CreateUserDto) {
+    return this.prismaService.user.create({
+      data: {
+        ...createUserDto,
+        password: bcrypt.hashSync(createUserDto.password, 10),
+      },
+    });
+  }
+
+  findAll() {
+    return this.prismaService.user.findMany();
+  }
+
+  findOne(id: string) {
+    return this.prismaService.user.findUnique({
+      where: { id },
+    });
+  }
+
+  update(id: string, updateUserDto: UpdateUserDto) {
+    return this.prismaService.user.update({
+      where: { id },
+      data: updateUserDto, //verificar se tem password e aplicar hash
+    });
+  }
+
+  remove(id: string) {
+    return this.prismaService.user.delete({
+      where: { id },
+    });
+  }
+}
